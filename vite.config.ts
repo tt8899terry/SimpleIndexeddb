aaa0745576ebe38bde1fb48a999e7a4ff76e700f
Andrew Tan <tt8899terry@gmail.com>
2024-09-11 01:52:49 +0800

    fixes #3 lib build types

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/SimpleIndexedDB/index.ts"),
-      name: "SimpleIndexedDB",
-      fileName: "SimpleIndexedDB",
+      name: "index",
+      // formats: ["es", "cjs"],
+      fileName: "index",
     },
   },
+  plugins: [
+    dts({
+      entryRoot: "src/SimpleIndexedDB", // Specify the root directory for entry files
+      rollupTypes: true, // This option merges all declarations into one file
+    }),
+  ],
 });
